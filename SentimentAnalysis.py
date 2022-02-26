05d7f09d02ec76e434ed75ab63868707ee0e43c1
shivamtiwari841797 <44071737+shivamtiwari841797@users.noreply.github.com>
2022-02-26 18:26:07 +0530

    Add files via upload

---
+++ SentimentAnalysis.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[69]:
+
+
+import pandas as pd
+data = pd.read_csv("C:/Users/Shipra/Documents/1429_1.csv",error_bad_lines=False, engine ='python', sep=',').head(n = 20)
+data
+
+
+# In[70]:
+
+
+import re
+def clean(text):
+# Removes all special characters and numericals leaving the alphabets
+#     print(type(text))
+    if type(text) == str:
+        text = re.sub('[^A-Za-z]+', ' ', text)
+    else:
+        text = ' '
+    
+    return text
+
+# Cleaning the text in the review column
+
+
+# In[71]:
+
+
+data['Cleaned Reviews'] = data['reviews.text'].apply(clean)
+data
+
+
+# In[72]:
+
+
+import nltk
+nltk.download('punkt')
+from nltk.tokenize import word_tokenize
+from nltk import pos_tag
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+nltk.download('wordnet')
+from nltk.corpus import wordnet
+
+# POS tagger dictionary
+pos_dict = {'J':wordnet.ADJ, 'V':wordnet.VERB, 'N':wordnet.NOUN, 'R':wordnet.ADV}
+def token_stop_pos(text):
+    tags = pos_tag(word_tokenize(text))
+    newlist = []
+    for word, tag in tags:
+        if word.lower() not in set(stopwords.words('english')):
+            newlist.append(tuple([word, pos_dict.get(tag[0])]))
+    return newlist
+
+data['POS tagged'] = data['Cleaned Reviews'].apply(token_stop_pos)
+data
+
+
+# In[73]:
+
+
+from nltk.stem import WordNetLemmatizer
+wordnet_lemmatizer = WordNetLemmatizer()
+def lemmatize(pos_data):
+    lemma_rew = " "
+    for word, pos in pos_data:
+        if not pos:
+            lemma = word
+            lemma_rew = lemma_rew + " " + lemma
+        else:
+            lemma = wordnet_lemmatizer.lemmatize(word, pos=pos)
+            lemma_rew = lemma_rew + " " + lemma
+    return lemma_rew
+
+data['Lemma'] = data['POS tagged'].apply(lemmatize)
+data
+
+
+# In[74]:
+
+
+from textblob import TextBlob
+# function to calculate subjectivity
+def getSubjectivity(review):
+    return TextBlob(review).sentiment.subjectivity
+    # function to calculate polarity
+def getPolarity(review):
+    return TextBlob(review).sentiment.polarity
+
+# function to analyze the reviews
+def analysis(score):
+    if score < 0:
+        return 'Negative'
+    elif score == 0:
+        return 'Neutral'
+    else:
+        return 'Positive'
+
+
+# In[77]:
+
+
+fin_data = pd.DataFrame(data[['reviews.text', 'Lemma']])
+
+
+# In[4]:
+
+
+fin_data['Subjectivity'] = fin_data['Lemma'].apply(getSubjectivity) 
+fin_data['Polarity'] = fin_data['Lemma'].apply(getPolarity) 
+fin_data['Analysis'] = fin_data['Polarity'].apply(analysis)
+fin_data
+
+
+# In[76]:
+
+
+tb_counts = fin_data.Analysis.value_counts()
+
+tb_counts
+
+
+# In[ ]:
+
+
+
+
